5eb7345a29c35791bd4358b588bc2d4dea754cf3
JPMC <56080352+jp-bitcube@users.noreply.github.com>
2020-10-24 22:39:41 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'my-dream-app';
+  demoForm: FormGroup;
+  director: {
+    name: string,
+    number: number,
+    email: string,
+  };
+  directorForm
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.demoForm = this.fb.group({
+      randomInput: [''],
+      directors: [[]]
+    });
+    this.director = { name: null, number: null, email: null };
+    this.addFormGroupToFormArray();
+    console.log(this.demoForm.get('directors'));
+  }
+
+  addFormGroupToFormArray(): void {
+    const demo = this.demoForm.get('directors').value;
+    const director = this.fb.group({});
+    demo.push(director);
+    Object.keys(this.director).forEach(field => {
+      console.log(field);
+      const control = this.fb.control(null, [Validators.required]);
+      demo[demo.length - 1].addControl(field, control);
+    });
+  }
+
+  removeFormGroupToFormArray(i: number): void {
+    const demo = this.demoForm.get('directors').value;
+    demo.splice(i, 1);
+  }
 }
